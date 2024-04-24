84f7603424e13bae6d20f31bedb7128b03b8212c
CrepsMan <eggenmattias+GitHub@gmail.com>
2024-04-24 14:47:17 +0200

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -317,7 +317,7 @@ function moveBirdTouch(e) {
 // Function to handle bird jumping
 function jump() {
     if (!gameOver) {
-        velocityY = isMobile ? -6 : -6; // Adjusted jump velocity for mobile devices
+        velocityY = isMobile ? -5 : -6; // Adjusted jump velocity for mobile devices
     } else {
         resetGame(); // Reset the game after game over
     }
